fa09fd266eb403dc24501f71781cdd0ef9932e03
Cade Scroggins <git@cade.me>
2020-11-28 11:54:34 +0530

    fix env context when no env context file attempt 2

--- bin/a-cdk.js
+++ bin/a-cdk.js
@@ -10,27 +10,30 @@ const ACdkStack = require('../lib/stacks/a-cdk.js');
 const formatResourceId = require('../lib/utilities/format-resource-id');
 
 const env = process.env.ENV;
-const contextFile = path.join(process.cwd(), `cdk.context.json`);
+const defaultContextFile = path.join(process.cwd(), `cdk.context.default.json`);
 const envContextFile = path.join(process.cwd(), `cdk.context.${env}.json`);
-let context = JSON.parse(fs.readFileSync(contextFile, 'utf8'));
 
-if (fs.existsSync(envContextFile)) {
-  context = deepmerge(
-    context,
-    JSON.parse(fs.readFileSync(envContextFile, 'utf8'))
-  );
-}
+const app = new cdk.App({
+  context: deepmerge(
+    fs.existsSync(defaultContextFile)
+      ? JSON.parse(fs.readFileSync(defaultContextFile, 'utf8'))
+      : {},
+    fs.existsSync(envContextFile)
+      ? JSON.parse(fs.readFileSync(envContextFile, 'utf8'))
+      : {}
+  ),
+});
 
-const app = new cdk.App({ context });
+const namespace = app.node.tryGetContext('namespace');
 
 if (!env) {
   console.error('Please specify an env! (via "ENV" environment variable)');
   process.exit(1);
 }
 
-if (!context.namespace) {
+if (!namespace) {
   console.error('Please specify a namespace! (via "namespace" context)');
   process.exit(1);
 }
 
-new ACdkStack(app, formatResourceId(context.namespace, env));
+new ACdkStack(app, formatResourceId(namespace, env));
