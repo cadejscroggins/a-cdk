c804163d086eac601b9080e2fbff4ac59b76aa92
Cade Scroggins <git@cade.me>
2020-11-28 10:53:02 +0530

    env specific context

--- bin/a-cdk.js
+++ bin/a-cdk.js
@@ -1,13 +1,26 @@
 #!/usr/bin/env node
+
 /* eslint-disable no-console */
 
 const cdk = require('@aws-cdk/core');
+const deepmerge = require('deepmerge');
+const fs = require('fs');
+const path = require('path');
 const ACdkStack = require('../lib/stacks/a-cdk.js');
 const formatResourceId = require('../lib/utilities/format-resource-id');
 
 const app = new cdk.App();
 const env = app.node.tryGetContext('env');
 const namespace = app.node.tryGetContext('namespace');
+const envContextFile = path.join(process.cwd(), `cdk.context.${env}.json`);
+
+if (fs.existsSync(envContextFile)) {
+  Object.entries(
+    fs.readFileSync(envContextFile).toJSON()
+  ).forEach(([key, value]) =>
+    app.node.setContext(key, deepmerge(app.node.tryGetContext(key), value))
+  );
+}
 
 if (!env) {
   console.error('Please specify an environment! (via "env" context)');
